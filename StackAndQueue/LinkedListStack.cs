2bade0c4c58542bc85b9d3ec082290cc31513a8b
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-19 17:15:20 +0530

    [Piyush] added ability to peak and pop from stack and till is empty

--- StackAndQueue/LinkedListStack.cs
+++ StackAndQueue/LinkedListStack.cs
@@ -26,7 +26,36 @@ namespace StackAndQueue
 				temp = temp.next;
 			}
 		}
-
-	}
+        //Peek method to show top element
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+		//Pop method to delete element
+		internal void Pop()
+		{
+			if (this.top == null)
+			{
+				Console.WriteLine("Stack is empty, Deletion is not possible ");
+				return;
+			}
+			Console.WriteLine("Value popped is {0} ",this.top.data);
+			this.top = this.top.next;
+		}
+		//Empty method to make stack empty
+		internal void IsEmpty()
+		{
+			while (this.top != null)
+			{
+				Peek();
+				Pop();
+			}
+		}
+    }
 }
 
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,11 +5,30 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Data Structure - Stack And Queue");
-        Console.WriteLine("Create Stack using Linked List");
+        Console.WriteLine("\n1.Create Stack \n2.Peek And Pop");
         LinkedListStack stack = new LinkedListStack();
-        stack.Push(70);
-        stack.Push(30);
-        stack.Push(56);
-        stack.Display();
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Create a Stack using Linked List");
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+                stack.Display();
+                break;
+            case 2:
+                Console.WriteLine("Peek and Pop from Stack");
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+                stack.Display();
+                stack.Peek();
+                stack.Pop();
+                stack.IsEmpty();
+                stack.Display();
+                break;
+        }
+        
     }
 }
